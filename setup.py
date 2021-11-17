9493a073654f35b7511298a3120d31123a920845
Rahul Samant <rahul.samant07@gmail.com>
2021-11-17 15:57:37 +0530

    Build Distribution and Travis CI Pipeline

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Coding-Assesment',
+    version='1.0',
+    packages=['lib'],
+    url='',
+    license='',
+    author='rahul',
+    author_email='rahul.samant07@gmail.com',
+    description='Coding Assesment Package'
+)
